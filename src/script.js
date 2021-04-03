372e38138796d29357a59d88162ecdfa426b5dbc
gustavodocarmokamitani <gustavodocarmokamitani@gmail.com>
2021-04-03 20:30:01 -0300

    All Pages

---
+++ src/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    const slickHome = {
+        autoplay: true,
+        dots: false,
+        prevArrow: '<button type="button" class="slick-prev slider__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next slider__next-arrow">Previous</button>;',
+    }
+
+    const slickTestimony = {
+        autoplay: true,
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev testimony__prev-arrow">Previous</button>;',
+        nextArrow: '<button type="button" class="slick-next testimony__next-arrow">Previous</button>;',
+    }
+
+    $('.slider').slick(slickHome);
+    $('.testimony').slick(slickTestimony);
+
+    $('.footer__form-button').on('click', () => {
+        const email = $("#email").val();
+        Email.send({
+            Host: "smtp.yourisp.com",
+            Username: "username",
+            Password: "password",
+            To: 'them@website.com',
+            From: email,
+            Subject: "Please add me to your newsletter",
+            Body: `Olá, 
+            Eu gostaria de ser adicionado a News Letter do site.
+            Meu email é ${email}.
+            Obrigado! `
+        }).then(
+            message => alert(message)
+        );
+    })
+});
